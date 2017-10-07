59c2901dcdd20f19a64ec5848cc2ec113bd5eb00
Chris Shaw <chrisstevenshaw@gmail.com>
2017-10-07 18:00:03 -0500

    write to csv

--- run.js
+++ run.js
@@ -1,8 +1,41 @@
+const fs = require('fs')
+const json2csv = require('json2csv')
 const config = require('./config.js')
 const Trello = require('node-trello')
 const productBoardId = 'V9Zv34WQ'
 
 const trello = new Trello(config.key, config.token)
 
-const results = trello.get(`/1/boards/${productBoardId}/actions`)
-console.log(JSON.stringify(results, null, 4))
+const run = () => {
+    const getListActivity = id => {
+        trello.get(
+            `/1/boards/${productBoardId}/actions`,
+            { fields: "date,type,memberCreator,data", memberCreator_fields: "username", filter: "updateCard:idList", actions_entities: true },
+            (err, data) => {
+                if (err) {
+                    console.log(err.toString())
+                    return err
+                } else {
+                    // console.log(JSON.stringify(data, null, 4))
+                    const csvData = data.map( action => {
+                        return {
+                            timestamp: action.date,
+                            card: action.data.card.name,
+                            endList: action.data.listBefore.name,
+                            startList: action.data.listAfter.name,
+                            teamMember: action.memberCreator.username
+                        }
+                    })
+                    // const csv = json2csv({ data: csvData })
+                    console.log(csvData)
+                    // fs.writeFile(`${__dirname}/movesToLists.csv`, csv, (err) => {
+                    //     if (err) throw err
+                    //     console.log('Success.')
+                    // })
+                }
+            }
+        )
+    }
+}
+
+run()
